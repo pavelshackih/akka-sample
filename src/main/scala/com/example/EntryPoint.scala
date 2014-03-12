203d10242c5106bc5dbb412fc6bb0d3567dfd8b3
Pavel Shackih <pavel.shackih@gmail.com>
2014-03-12 21:59:53 +0400

    try calculate using tree

--- src/main/scala/com/example/EntryPoint.scala
+++ src/main/scala/com/example/EntryPoint.scala
@@ -1,68 +1,116 @@
 package com.example
 
 import akka.actor._
+import scala.Some
 
 object EntryPoint extends App {
 
   val system = ActorSystem("system")
 
-  val n = 10
+  val n = 16
+  val broadNodes = n / 2
 
-  val randomUpperBound = 100
+  val randomUpperBound = 10
   val random = util.Random
+
   def getRandom = random.nextInt(randomUpperBound)
 
-  val master = system.actorOf(Props(new Master(n)), name = "master")
-  master ! StartSumCalc
+  val randomValues = 0 until n map(x => getRandom)
+  val expectedSum = randomValues.reduce(_ + _)
+  println(s"Expected sum: $expectedSum")
 
-  class Master(n: Int) extends Actor {
+  val listener = system.actorOf(Props(new Listener(n)), name = "listener")
 
-    var cluster = new collection.mutable.ArrayBuffer[ActorRef]()
-    for (i <- 0 until n) {
-      cluster += context.actorOf(Props[Node])
-    }
+  var cluster = new collection.mutable.ArrayBuffer[ActorRef]()
+  for (i <- 0 until n) {
+    cluster += system.actorOf(Props(new BiNode(listener, n, i)))
+  }
 
-    def broadcast(obj: Any) = cluster.foreach(_ ! obj)
+  // start calculating from childs that to parents
+  for (i <- (n - 1) to broadNodes by -1) {
+    cluster(i) ! SendToANode(0)
+  }
 
-    var sum = 0
-    var messages = 0
+  class BiNode(listener: ActorRef, val n: Int, id: Int) extends Actor with ActorLogging {
+
+    var i = randomValues(id)
+
+    val childs = new collection.mutable.HashMap[Child, ActorRef]
+    var visited = 0
+
+    left.map(childs.put(Left, _))
+    right.map(childs.put(Right, _))
+    val hasParent = id != 0
+
+    var globalSum = 0
+    var propagated = false
 
     override def receive = {
-      case StartSumCalc => broadcast(ReportPrivateNum)
-      case SendToANode(i) =>
-        messages += 1
-        sum += i
-        if (messages == n) {
-          messages = 0
-          broadcast(SendToANode(sum))
+      case SendToANode(value) =>
+        i += value
+        if (propagated) {
+          globalSum = value
+          log.info(s"Sum: $globalSum")
+          childs.values.foreach(_ ! SendToANode(globalSum))
+          fireSumUpdated()
+          context stop self
+        } else {
+          visited += 1
+          if (visited >= childs.size) { // waiting when both of childs returns they values
+            propagated = true
+            if (hasParent) {
+              parent.map(_ ! SendToANode(i))
+            } else {
+              // sum calculated, propagate value to childs
+              globalSum = i
+              log.info(s"Sum: $globalSum")
+              fireSumUpdated()
+              childs.values.foreach(_ ! SendToANode(globalSum))
+              context stop self
+            }
+          }
         }
-      case SumAccepted =>
-        messages += 1
-        if (messages == n) {
+    }
+
+    def fireSumUpdated() = listener ! Finished
+
+    def parent = {
+      val inc = if (id % 2 == 0) 2 else 1
+      val index = (id - inc) / 2
+      if (index > -1) Some(cluster(index)) else None
+    }
+
+    def left = {
+      val ix = 2 * id + 1
+      if (ix > cluster.size - 1) None else Some(cluster(ix))
+    }
+
+    def right = {
+      val ix = 2 * id + 2
+      if (ix > cluster.size - 1) None else Some(cluster(ix))
+    }
+
+    def hasChilds = id < n / 2 + 1
+  }
+
+  class Listener(n: Int) extends Actor with ActorLogging {
+
+    var counter = 0
+
+    override def receive = {
+      case Finished =>
+        counter += 1
+        if (counter == n) {
+          log.info("Completed")
           system.shutdown()
         }
     }
   }
 
-  class Node extends Actor with ActorLogging {
-
-    val i = getRandom
-    var sum: Int = 0
-
-    override def receive = {
-      case ReportPrivateNum =>
-        sender ! SendToANode(i)
-      case SendToANode(sumFromMaster) =>
-        sum = sumFromMaster
-        log.info(s"Sum of all nodes: $sum")
-        sender ! SumAccepted
-        context stop self
-    }
-  }
-
   case class SendToANode(message: Int)
-  case object StartSumCalc
-  case object ReportPrivateNum
-  case object SumAccepted
+  case object Finished
+  trait Child
+  case object Left extends Child
+  case object Right extends Child
 
 }
